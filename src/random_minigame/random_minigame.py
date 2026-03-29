c79229c88d0bcda6281b394ecc46b619fa09f894
Jerry Wang <bw2471@nyu.edu>
2026-03-28 22:36:29 -0400

    Add choose_option function and tests

--- src/random_minigame/random_minigame.py
+++ src/random_minigame/random_minigame.py
@@ -2,19 +2,7 @@ import random
 
 
 def choose_option(choices):
-    """
-    Randomly choose one option from a non-empty list.
-
-    Args:
-        choices (list): A list of possible options.
-
-    Returns:
-        One randomly selected item from the list.
-
-    Raises:
-        TypeError: If choices is not a list.
-        ValueError: If choices is empty.
-    """
+    
     if not isinstance(choices, list):
         raise TypeError("choices must be a list")
     if not choices:
